15f4cda9ed7ca1e0150d0229feca124dc80cf710
Pablo Grant <grantpablo4@gmail.com>
2020-07-29 13:11:29 -0500

    fixed franchise controller to search by both id and owner

--- lib/haberdash_web/controllers/franchise_controller.ex
+++ lib/haberdash_web/controllers/franchise_controller.ex
@@ -13,57 +13,53 @@ defmodule HaberdashWeb.FranchiseController do
 
   def create(conn, %{"franchise" => franchise_params}) do
     with %Owner{id: id} <- Auth.Guardian.Plug.current_resource(conn),
-         {:ok, %Franchise{} = franchise} <- Business.create_franchise(franchise_params |> Enum.into(%{"owner_id" => id})) do
+         {:ok, %Franchise{} = franchise} <-
+           Business.create_franchise(franchise_params |> Enum.into(%{"owner_id" => id})) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", Routes.franchise_path(conn, :show, franchise))
       |> render("show.json", franchise: franchise)
     end
   end
 
-
-  def show(conn, _params) do
-    with %Owner{id: id} <- Auth.Guardian.Plug.current_resource(conn),
-         {:ok, %Franchise{} = franchise} <- Business.get_franchise_by(owner_id: id) do
-          render(conn, "show.json", franchise: franchise)
-    else
-      nil ->
-          conn
-          |> resp(
-            :unauthorized,
-            Poison.encode!(%{
-              code: :unauthorized,
-              message:
-                "You aren't authorized to access this route. This may be because you aren't currently logged in or your session expired due to inactivity. "
-            })
-          ) |> send_resp()
-      {:error, :not_found} ->
-        {:error, :not_found}
-        error ->
-          error
-
-
-
-    end
-
-
-  end
-
-  def update(conn, %{"franchise" => franchise_params}) do
-    %Owner{id: id} = owner = Auth.Guardian.Plug.current_resource(conn)
-    {:ok, franchise} = Business.get_franchise_by(owner_id: id)
-    with {:ok, %Franchise{} = franchise} <- Business.update_franchise(franchise, franchise_params) do
+  def show(conn, %{"id" => id}) do
+    with %Owner{id: owner_id} <- Auth.Guardian.Plug.current_resource(conn),
+         {:ok, %Franchise{} = franchise} <- Business.get_franchise_by(owner_id: owner_id, id: id) do
       render(conn, "show.json", franchise: franchise)
     else
+      nil ->
+        conn
+        |> resp(
+          :unauthorized,
+          Poison.encode!(%{
+            code: :unauthorized,
+            message:
+              "You aren't authorized to access this route. This may be because you aren't currently logged in or your session expired due to inactivity. "
+          })
+        )
+        |> send_resp()
+
+      {:error, :not_found} ->
+        {:error, :not_found}
+
       error ->
         error
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    franchise = Business.get_franchise!(id)
+  def update(conn, %{"id" => id, "franchise" => franchise_params}) do
+    %Owner{id: owner_id} = Auth.Guardian.Plug.current_resource(conn)
+    {:ok, franchise} = Business.get_franchise_by(owner_id: owner_id, id: id)
 
-    with {:ok, %Franchise{}} <- Business.delete_franchise(franchise) do
+    with {:ok, %Franchise{} = franchise} <- Business.update_franchise(franchise, franchise_params) do
+      render(conn, "show.json", franchise: franchise)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    %Owner{id: owner_id} = Auth.Guardian.Plug.current_resource(conn)
+
+    with {:ok, franchise} = Business.get_franchise_by(id: id, owner_id: owner_id),
+         {:ok, %Franchise{}} <- Business.delete_franchise(franchise) do
       send_resp(conn, :no_content, "")
     end
   end
