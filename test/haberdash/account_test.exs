acf8dbfe43c426d24d0de8afcd5e5e41d64842ea
Pablo Grant <grantpablo4@gmail.com>
2020-07-28 23:29:58 -0500

    fixed all of the errors in account test for now

--- test/haberdash/account_test.exs
+++ test/haberdash/account_test.exs
@@ -7,15 +7,15 @@ defmodule Haberdash.AccountTest do
     alias Haberdash.Account.Owner
 
     @valid_attrs %{
-      email: "some email",
+      email: "some@email.com",
       name: "some name",
-      phone_number: "some phone_number",
+      phone_number: "+4915843854",
       password: "some password"
     }
     @update_attrs %{
-      email: "some updated email",
+      email: "someupdated@email.com",
       name: "some updated name",
-      phone_number: "some updated phone_number",
+      phone_number: "+4915843850",
       password: "some password"
     }
     @invalid_attrs %{email: nil, name: nil, phone_number: nil}
@@ -41,9 +41,9 @@ defmodule Haberdash.AccountTest do
 
     test "create_owner/1 with valid data creates a owner" do
       assert {:ok, %Owner{} = owner} = Account.create_owner(@valid_attrs)
-      assert owner.email == "some email"
+      assert owner.email == "some@email.com"
       assert owner.name == "some name"
-      assert owner.phone_number == "some phone_number"
+      assert owner.phone_number == "+4915843854"
     end
 
     test "create_owner/1 with invalid data returns error changeset" do
@@ -53,9 +53,9 @@ defmodule Haberdash.AccountTest do
     test "update_owner/2 with valid data updates the owner" do
       owner = owner_fixture()
       assert {:ok, %Owner{} = owner} = Account.update_owner(owner, @update_attrs)
-      assert owner.email == "some updated email"
+      assert owner.email == "someupdated@email.com"
       assert owner.name == "some updated name"
-      assert owner.phone_number == "some updated phone_number"
+      assert owner.phone_number == "+4915843850"
     end
 
     test "update_owner/2 with invalid data returns error changeset" do
@@ -78,26 +78,34 @@ defmodule Haberdash.AccountTest do
 
   describe "developer" do
     alias Haberdash.Account.Developer
-
+    @owner_attrs %{
+      email: "some@email.com",
+      name: "some name",
+      phone_number: "+4915843854",
+      password: "some password"
+    }
     @valid_attrs %{
       api_key: "some api_key",
-      email: "some email",
+      email: "some@email.com",
       name: "some name",
       password_hash: "some password_hash"
     }
     @update_attrs %{
       api_key: "some updated api_key",
-      email: "some updated email",
+      email: "someupdated@email.com",
       name: "some updated name",
       password_hash: "some updated password_hash"
     }
     @invalid_attrs %{api_key: nil, email: nil, name: nil, password_hash: nil}
 
     def developers_fixture(attrs \\ %{}) do
+      {:ok, owner} = Account.create_owner(@owner_attrs)
+
       {:ok, developers} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Account.create_developers()
+        |> Enum.into(%{owner_id: owner.id})
+        |> Account.create_developer()
 
       developers
     end
@@ -109,48 +117,49 @@ defmodule Haberdash.AccountTest do
 
     test "get_developers!/1 returns the developers with given id" do
       developers = developers_fixture()
-      assert Account.get_developers!(developers.id) == developers
+      assert Account.get_developer!(developers.id) == developers
     end
 
     test "create_developers/1 with valid data creates a developers" do
-      assert {:ok, %Developer{} = developers} = Account.create_developers(@valid_attrs)
+      assert {:ok, %Account.Owner{id: id}} = Account.create_owner(@owner_attrs)
+      assert {:ok, %Developer{} = developers} = Account.create_developer(@valid_attrs |> Enum.into(%{owner_id: id}))
       assert developers.api_key == "some api_key"
-      assert developers.email == "some email"
+      assert developers.email == "some@email.com"
       assert developers.name == "some name"
       assert developers.password_hash == "some password_hash"
     end
 
     test "create_developers/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Account.create_developers(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Account.create_developer(@invalid_attrs)
     end
 
     test "update_developers/2 with valid data updates the developers" do
       developers = developers_fixture()
 
       assert {:ok, %Developer{} = developers} =
-               Account.update_developers(developers, @update_attrs)
+               Account.update_developer(developers, @update_attrs)
 
       assert developers.api_key == "some updated api_key"
-      assert developers.email == "some updated email"
+      assert developers.email == "someupdated@email.com"
       assert developers.name == "some updated name"
       assert developers.password_hash == "some updated password_hash"
     end
 
     test "update_developers/2 with invalid data returns error changeset" do
       developers = developers_fixture()
-      assert {:error, %Ecto.Changeset{}} = Account.update_developers(developers, @invalid_attrs)
-      assert developers == Account.get_developers!(developers.id)
+      assert {:error, %Ecto.Changeset{}} = Account.update_developer(developers, @invalid_attrs)
+      assert developers == Account.get_developer!(developers.id)
     end
 
     test "delete_developers/1 deletes the developers" do
       developers = developers_fixture()
-      assert {:ok, %Developer{}} = Account.delete_developers(developers)
-      assert_raise Ecto.NoResultsError, fn -> Account.get_developers!(developers.id) end
+      assert {:ok, %Developer{}} = Account.delete_developer(developers)
+      assert_raise Ecto.NoResultsError, fn -> Account.get_developer!(developers.id) end
     end
 
     test "change_developers/1 returns a developers changeset" do
       developers = developers_fixture()
-      assert %Ecto.Changeset{} = Account.change_developers(developers)
+      assert %Ecto.Changeset{} = Account.change_developer(developers)
     end
   end
 end
