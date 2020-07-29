c56dbb1a0932cb46fe7cbf5a7c3d8ee067b6ce64
Pablo Grant <grantpablo4@gmail.com>
2020-07-29 01:36:36 -0500

    added type: :binary_id to owner relationship

--- lib/haberdash/account/developer.ex
+++ lib/haberdash/account/developer.ex
@@ -2,13 +2,13 @@ defmodule Haberdash.Account.Developer do
   use Ecto.Schema
   import Ecto.Changeset
   alias Haberdash.{Account}
-
+  @primary_key {:id, :binary_id, autogenerate: true}
   schema "developer" do
     field :api_key, :string
     field :email, :string
     field :name, :string
     field :password_hash, :string
-    belongs_to :owner, Account.Owner
+    belongs_to :owner, Account.Owner, type: :binary_id
 
     timestamps()
   end
