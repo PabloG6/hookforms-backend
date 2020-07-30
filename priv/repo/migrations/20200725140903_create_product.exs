9d732f6968d0e827b43d5a6f4bae3adfb5c64cc5
Pablo Grant <grantpablo4@gmail.com>
2020-07-29 21:20:33 -0500

    fixed product id to use uuid instead of bigserial ids

--- priv/repo/migrations/20200725140903_create_product.exs
+++ priv/repo/migrations/20200725140903_create_product.exs
@@ -2,12 +2,13 @@ defmodule Haberdash.Repo.Migrations.CreateProduct do
   use Ecto.Migration
 
   def change do
-    create table(:product) do
+    create table(:product, primary_key: false) do
       add :name, :string
       add :price, :decimal
       add :description, :text
       add :price_id, :string
       add :franchise_id, references(:franchise, on_delete: :delete_all, type: :binary_id)
+      add :id, :binary_id, primary_key: true
       timestamps()
     end
 
