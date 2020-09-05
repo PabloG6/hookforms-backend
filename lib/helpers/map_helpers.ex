28f78949aedfaab6d428037f250d510a1b930d53
Pablo Grant <grantpablo4@gmail.com>
2020-09-05 14:29:58 -0500

    capture structs and remove associations from ecto schemas

--- lib/helpers/map_helpers.ex
+++ lib/helpers/map_helpers.ex
@@ -6,8 +6,17 @@ defmodule Haberdash.MapHelpers do
 
       def stringify_map(map) when is_map(map), do: stringify_map(map, 0)
 
+      defp stringify_map(%{__meta__: _} = map, depth) do
+        # Logger.info("is meta hanging up everything map: #{inspect(map)}")
+        Map.drop(map, map.__struct__.__schema__(:associations) ++ [:__meta__]) |> Map.from_struct|> stringify_map(depth)
+      end
       defp stringify_map(%NaiveDateTime{} = map, _) do
         map
+      end
+
+      defp stringify_map(%{__struct__: _} = map, depth) do
+        Logger.info("map is strucct")
+        Map.from_struct(map) |> stringify_map(depth)
       end
       defp stringify_map(map, depth) when is_map(map) and depth < 2 do
         Logger.info("map: #{inspect(map)} depth: #{depth}")
@@ -23,6 +32,8 @@ defmodule Haberdash.MapHelpers do
       end
 
       defp stringify_map(map, _), do: map
+
+
       defp safe_atom_to_string(key) when is_atom(key), do: Atom.to_string(key)
       defp safe_atom_to_string(key) when is_binary(key), do: key
       defp safe_atom_to_string(_), do: raise(ArgumentError, message: "key isn't a binary or atom")
