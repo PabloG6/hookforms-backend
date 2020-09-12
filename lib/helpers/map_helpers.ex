a45391cbbc02a345e41da8ce25fbfa79b25f2e67
Pablo Grant <grantpablo4@gmail.com>
2020-09-12 14:02:41 -0500

    added reduce funtion to find the total of the price in the map

--- lib/helpers/map_helpers.ex
+++ lib/helpers/map_helpers.ex
@@ -4,6 +4,22 @@ defmodule Haberdash.MapHelpers do
       # helper functions
       require Logger
 
+      #recursively iterates over an enumerable
+      def reduce(enumerable, acc, func), do: reduce_func(enumerable, acc, func)
+
+      defp reduce_func(struct, acc, _) when is_struct(struct), do: acc
+      defp reduce_func(enumerable, acc, fun) when is_map(enumerable) do
+        Enum.reduce(enumerable, acc, fn x, acc ->  reduce_func(x, acc, fun) end)
+      end
+
+      defp reduce_func(enumerable, acc, fun) when is_list(enumerable) do
+        Enum.reduce(enumerable, acc, fn x, acc -> reduce_func(x, acc, fun) end)
+      end
+
+      defp reduce_func({k, v} = enumerable, acc, func) when is_tuple(enumerable), do: reduce_func(v, func.(enumerable, acc), func)
+      defp reduce_func(_, acc, _), do: acc
+
+      #converts atom maps to string with a depth of 2
       def stringify_map(map) when is_map(map), do: stringify_map(map, 0)
 
       defp stringify_map(%{__meta__: _} = map, depth) do
@@ -28,7 +44,7 @@ defmodule Haberdash.MapHelpers do
 
       defp stringify_map(enum, depth) when is_list(enum) do
         for element <- enum do
-          stringify_map(element, depth + 1)
+          stringify_map(element, 0)
         end
       end
 
@@ -37,6 +53,7 @@ defmodule Haberdash.MapHelpers do
       defp safe_atom_to_string(key) when is_atom(key), do: Atom.to_string(key)
       defp safe_atom_to_string(key) when is_binary(key), do: key
       defp safe_atom_to_string(_), do: raise(ArgumentError, message: "key isn't a binary or atom")
+
     end
   end
 end
