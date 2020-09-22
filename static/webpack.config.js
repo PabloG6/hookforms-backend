9bbe94682bcda792bd725ebd6e182343fab6d4b5
Pablo Grant <grantpablo4@gmail.com>
2020-09-22 00:25:25 -0500

    added webpack and material-components-web

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,20 +1,59 @@
-module.exports = {
-    entry: "app.ts",
+path = require("path")
+module.exports =
+{
+    entry: ["./styles.scss", "./app.ts"],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js',
+        filename: 'bundle.js',
     },
 
     module: {
         rules: [
             {
-                test: 'tsx',
+                test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: './styles/bundle.css',
+                        }
+                    },
+
+                    {
+                        loader: 'extract-loader'
+                    },
+
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            webpackImporter: false,
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            },
+
+                        }
+                    },
+
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                },
             }
+
         ]
     },
     resolve: {
-        
+        extensions: ['.tsx', '.ts', '.js']
     }
-}
+};
