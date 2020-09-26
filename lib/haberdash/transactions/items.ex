6e98ae6d52a1fb48680978e4fe7db158ac2ac34a
Pablo Grant <grantpablo4@gmail.com>
2020-09-25 20:46:18 -0500

    chore: changed some variable values

--- lib/haberdash/transactions/items.ex
+++ lib/haberdash/transactions/items.ex
@@ -5,18 +5,16 @@ defmodule Haberdash.Transactions.OrderItems do
   @primary_key {:id, :binary_id, autogenerate: true}
   embedded_schema do
     field :name, :string
-    field :item_id, :binary_id
+    field :inventory_id, :binary_id
     field :description, :string
     field :price, :integer
     field :message, :string, virtual: true
-    field :item_type, Haberdash.Transactions.ItemType, default: :products
     embeds_many :accessories, Transactions.Accessories
   end
 
   def changeset(item, attrs) do
     item
-    |> cast(attrs, [:name, :item_id, :description, :price, :item_type])
-    |> validate_required([:item_type])
+    |> cast(attrs, [:name, :inventory_id, :description, :price])
     |> cast_embed(:accessories)
   end
 end
