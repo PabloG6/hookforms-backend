0e23b6dfa43f833f074528f3df045470e030d651
Pablo Grant <grantpablo4@gmail.com>
2021-10-29 12:37:18 -0500

    add regex to cors

--- lib/forms_web/plugs/cors.ex
+++ lib/forms_web/plugs/cors.ex
@@ -1,6 +1,6 @@
 defmodule Forms.Cors do
   use Corsica.Router,
-    origins: ["https://hookforms.dev"],
+    origins: [~r{^https?://(.*\.?)hookforms\.dev}],
     allow_credentials: true,
     log: [rejected: :error],
     allow_headers: :all,
