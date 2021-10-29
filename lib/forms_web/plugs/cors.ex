f91b744591b45578bffd1d703fbf6714ec9668bb
Pablo Grant <grantpablo4@gmail.com>
2021-10-29 12:24:26 -0500

    fix (cors): allow all headers and methods in cors

--- lib/forms_web/plugs/cors.ex
+++ lib/forms_web/plugs/cors.ex
@@ -2,5 +2,7 @@ defmodule Forms.Cors do
   use Corsica.Router,
     origins: ["https://hookforms.dev"],
     allow_credentials: true,
-    log: [rejected: :error]
+    log: [rejected: :error],
+    allow_headers: :all,
+    allow_methods: :all
 end
