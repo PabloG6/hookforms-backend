dc7c5a4750229bcd89d6bbc6e45ed24aed8976e5
Pablo Grant <grantpablo4@gmail.com>
2021-11-03 20:19:08 -0500

    fix: add database configuration

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,31 +4,31 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-# database_url =
-#   System.get_env("DATABASE_URL") ||
-#     raise """
-#     environment variable DATABASE_URL is missing.
-#     For example: ecto://USER:PASS@HOST/DATABASE
-#     """
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
 
-# config :forms, Forms.Repo,
-#   # ssl: true,
-#   url: database_url,
-#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+config :forms, Forms.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-# secret_key_base =
-#   System.get_env("SECRET_KEY_BASE") ||
-#     raise """
-#     environment variable SECRET_KEY_BASE is missing.
-#     You can generate one by calling: mix phx.gen.secret
-#     """
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
-# guardian_secret_key =
-#   System.get_env("GUARDIAN_SECRET_KEY") ||
-#   raise """
-#   environment variable GUARDIAN_SECRET_KEY is missing.
-#   You can generate one by calling: mix phx.gen.secret
-#   """
+guardian_secret_key =
+  System.get_env("GUARDIAN_SECRET_KEY") ||
+  raise """
+  environment variable GUARDIAN_SECRET_KEY is missing.
+  You can generate one by calling: mix phx.gen.secret
+  """
 
 config :forms, FormsWeb.Endpoint,
   http: [
@@ -36,12 +36,12 @@ config :forms, FormsWeb.Endpoint,
 
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: "some nonsense",
+  secret_key_base: secret_key_base,
   server: true
 
 config :forms, Forms.Guardian,
   issuer: "forms",
-  secret_key: "some nonsense"
+  secret_key: guardian_secret_key
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
